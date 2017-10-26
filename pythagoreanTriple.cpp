d2dc280e93f575d6fab7b0bdf345c77d3f8b75b1
avantikasharma <33115340+avantikasharma@users.noreply.github.com>
2017-10-26 17:07:12 +0530

    Create pythagoreanTriple.cpp

---
+++ pythagoreanTriple.cpp
@@ -0,0 +1,25 @@
+#include<iostream.h>
+#include<conio.h>
+#inclulde<math.h>
+void main()
+{
+clrscr();
+char c='y';
+while(c=='y')
+{
+  float a,b,c;
+    cout<<"\nEnter three sides of the triangle: \n";
+    cin>>a>>b>>c;
+  if((pow(c,2)==pow(a,2)+pow(b,2))
+    cout<<"\nIt is a pythagorean triplet";
+  else if((pow(a,2)==pow(c,2)+pow(b,2))
+    cout<<"\nIt is a pythagorean triplet";
+  else if((pow(b,2)==pow(c,2)+pow(a,2))
+    cout<<"\nIt is a pythagorean triplet";
+  else
+    cout<<"\nIt is not a pythagorean triplet";
+  cout<<"Do you wish to continue? y/n";
+  cin>>c;
+ }
+ getch();
+ }
